79d2a7d524c2ccd5f3cdbd496f2e1601963b5257
abramdev <abramdev@163.com>
2026-01-17 10:53:05 +0800

    chore: configure semantic-release release rules

--- .releaserc.js
+++ .releaserc.js
@@ -1,7 +1,22 @@
 export default {
   branches: ["master"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "test", release: "patch" },
+          { type: "build", release: "patch" },
+          { type: "chore", release: "patch" },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
